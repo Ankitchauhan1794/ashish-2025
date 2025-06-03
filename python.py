044328611fb6badd2af224811223549ad12eea6f
ashish1108-devops <garg.ashish1108@gmail.com>
2025-06-03 17:28:58 +0530

    ashish

---
+++ python.py
@@ -0,0 +1,9 @@
+#1.	Write a program to generate all possible multiples of a number provided by the user.
+
+a=int(input("Enter the Number: "))
+num=int(input("Enter the range: "))
+m=int(input("Enter a no that you want to the multiples: "))
+while a<=num:
+    multi=a*m
+    print(multi)
+    a+=1
