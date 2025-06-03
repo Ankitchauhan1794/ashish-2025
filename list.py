32882c801b7ce35ca68ff6a4c223a185e487d7fa
ashish1108-devops <garg.ashish1108@gmail.com>
2025-06-03 17:04:37 +0530

    abc

---
+++ list.py
@@ -0,0 +1,11 @@
+my_list=[i for i in range(1, 10)]
+print(my_list)
+
+
+# echo "# ashish-2025" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin git@github.com:ashish1108-devops/ashish-2025.git
+# git push -u origin main
